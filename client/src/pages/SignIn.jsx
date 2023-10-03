ae4e3fdf0d2ed928a4b7e3418802a4e9465b0377
lehoangvinh <lhv.vinh88@gmail.com>
2023-10-03 10:13:55 +0700

    sign in

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,14 +1,23 @@
-import React, { useEffect, useState } from "react";
-
+import { useEffect, useState } from "react";
+import {
+  AiOutlineEye,
+  AiOutlineEyeInvisible,
+  AiOutlineClose,
+} from "react-icons/ai";
 export const SignUp = () => {
   const [isSignUp, setIsSignUp] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const [loginUser, setLoginUser] = useState({ username: "", password: "" });
   const [regUser, setRegUser] = useState({
     email: "",
     username: "",
     password: "",
   });
-
+  const [validation, setValidation] = useState({
+    email: true,
+    username: true,
+    password: true,
+  });
   useEffect(() => {
     const signUpButton = document.getElementById("signUp");
     const container = document.querySelector(".box");
@@ -16,9 +25,20 @@ export const SignUp = () => {
     signUpButton.addEventListener("click", () => {
       container.classList.toggle("right-panel-active");
       setIsSignUp((prev) => !prev);
+      setShowPassword(false);
+      setLoginUser({ username: "", password: "" });
+      setRegUser({ email: "", username: "", password: "" });
+      document.querySelector("input[name='password']").type = "password";
       document.querySelectorAll("input")?.forEach((item) => (item.value = ""));
     });
   }, []);
+
+  const handleShowPassword = () => {
+    setShowPassword((prev) => !prev);
+    !showPassword
+      ? (document.querySelector("input[name='password']").type = "text")
+      : (document.querySelector("input[name='password']").type = "password");
+  };
 
   const handleChange = (e) => {
     if (isSignUp) {
@@ -34,11 +54,50 @@ export const SignUp = () => {
   };
 
   const handleSubmit = (e) => {
+    if (isSignUp) {
+      if (
+        regUser.email === "" ||
+        regUser.email === null ||
+        regUser.email.length == 0
+      ) {
+        setValidation({ ...validation, email: false });
+      }
+      if (
+        regUser.username === "" ||
+        regUser.username === null ||
+        regUser.username.length == 0
+      ) {
+        setValidation({ ...validation, username: false });
+      }
+      if (
+        regUser.password === "" ||
+        regUser.password === null ||
+        regUser.password.length == 0
+      ) {
+        setValidation({ ...validation, password: false });
+      }
+    } else {
+      if (
+        loginUser.username === "" ||
+        loginUser.username === null ||
+        loginUser.username.length == 0
+      ) {
+        setValidation({ ...validation, username: false });
+      }
+      if (
+        loginUser.password === "" ||
+        loginUser.password === null ||
+        loginUser.password.length == 0
+      ) {
+        setValidation({ ...validation, password: false });
+      }
+    }
+
     e.preventDefault();
     if (isSignUp) console.log(regUser);
     else console.log(loginUser);
   };
-
+  console.log(validation);
   return (
     <div className="box">
       <div className="wrapper">
@@ -62,7 +121,7 @@ export const SignUp = () => {
                       ? "Join us today! Create your account."
                       : "Welcome back! Sign in to access your account."}
                   </p>
-                  <p className="p">
+                  <p className="p select-none">
                     {isSignUp
                       ? " Ready to get started? Fill out the form below to create your new account. It only takes a few moments !"
                       : "Enter your username and password below to securely sign in and enjoy all the features and benefits of our platform."}
@@ -86,15 +145,26 @@ export const SignUp = () => {
               <div className="text-wrapper-4">
                 {isSignUp ? "Create Account" : "Sign In To App"}
               </div>
+              <span className="bg-red-200 px-4 py-4 rounded w-[340px] font-medium flex items-center justify-center">
+                Incorrect username or password.{" "}
+                <span>
+                  <AiOutlineClose className="text-red-400 ml-2 cursor-pointer" />
+                </span>
+              </span>
               <div className="frame-5">
                 {isSignUp && (
-                  <input
-                    className="px-4 py-2 rounded-lg outline-none deep-neumorphism min-w-[340px]"
-                    type="email"
-                    placeholder="Email"
-                    name="email"
-                    onChange={handleChange}
-                  />
+                  <>
+                    <input
+                      className="px-4 py-2 rounded-lg outline-none deep-neumorphism min-w-[340px]"
+                      type="email"
+                      placeholder="Email"
+                      name="email"
+                      onChange={handleChange}
+                    />
+                    <span className="text-red-500 font-medium text-md px-4">
+                      Email must not be empty!.
+                    </span>
+                  </>
                 )}
                 <input
                   className="px-4 py-2 rounded-lg outline-none deep-neumorphism min-w-[340px]"
@@ -103,13 +173,46 @@ export const SignUp = () => {
                   name="username"
                   onChange={handleChange}
                 />
-                <input
-                  className="px-4 py-2 rounded-lg outline-none deep-neumorphism min-w-[340px]"
-                  type="password"
-                  placeholder="Password"
-                  name="password"
-                  onChange={handleChange}
-                />
+                {!validation.username && (
+                  <span className="text-red-500 font-medium text-md px-4">
+                    Username must not be empty!.
+                  </span>
+                )}
+                <div className="relative">
+                  <input
+                    className="px-4 py-2 rounded-lg outline-none deep-neumorphism min-w-[340px]"
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    onChange={handleChange}
+                  />
+
+                  <span
+                    className="absolute right-4 top-2.5 cursor-pointer"
+                    onClick={handleShowPassword}
+                  >
+                    {isSignUp ? (
+                      regUser.password !== "" ? (
+                        !showPassword ? (
+                          <AiOutlineEye size={24} />
+                        ) : (
+                          <AiOutlineEyeInvisible size={24} />
+                        )
+                      ) : null
+                    ) : loginUser.password !== "" ? (
+                      !showPassword ? (
+                        <AiOutlineEye size={24} />
+                      ) : (
+                        <AiOutlineEyeInvisible size={24} />
+                      )
+                    ) : null}
+                  </span>
+                </div>
+                {!validation.username && (
+                  <span className="text-red-500 font-medium text-md px-4">
+                    Password must not be empty!.
+                  </span>
+                )}
               </div>
               <>
                 <button
