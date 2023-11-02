2ab01b98d17bcf0bff8f527a02fdb884e5aeacc5
Minh <bhminh322@gmail.com>
2023-11-02 22:29:55 +0700

    fix: create same channel pm

--- server/src/main/java/com/chat/server/repository/custom/CustomizedChannelRepo.java
+++ server/src/main/java/com/chat/server/repository/custom/CustomizedChannelRepo.java
@@ -3,6 +3,7 @@ package com.chat.server.repository.custom;
 import com.chat.server.payload.response.ChannelInfo;
 import com.chat.server.payload.response.ChannelMessage;
 import com.chat.server.payload.response.SearchChannelResponse;
+import org.bson.types.ObjectId;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,4 +20,5 @@ public interface CustomizedChannelRepo {
     List<ChannelMessage> findAllChannelByUser(String userId);
 
     List<SearchChannelResponse> findByKeyword(String keyword, String userId);
+    boolean existChannelPM(ObjectId... userIds);
 }
